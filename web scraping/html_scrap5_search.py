d8cb3500c00f5ff7ba0b20c8c7403ba88612338f
Marxu <fuhaojun@52tt.com>
2024-07-23 14:44:46 +0800

    feat: python 输入一个关键字 自动搜索并打开多个网页

--- web scraping/html_scrap5_search.py
+++ web scraping/html_scrap5_search.py
@@ -1,11 +1,45 @@
-# Opens several search results.
+import requests
+import sys
+import webbrowser
+import bs4
 
-import requests, sys, webbrowser, bs4
-print('Searching...')
-res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
-+ ' '.join(sys.argv[1:]))
+# 检查是否有传入参数
+if len(sys.argv) < 2:
+    print("Usage: python script_name.py search_term")
+    sys.exit()
+
+search_term = ' '.join(sys.argv[1:])
+print('Searching for:', search_term)
+
+# 构造搜索 URL  搜索 pip 包
+url = f'https://pypi.org/search/?q={search_term}'
+
+# 发起请求
+res = requests.get(url)
 res.raise_for_status()
 
-# TODO: Retrieve top search result links.
+# 解析 HTML
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-# TODO: Open a browser tab for each result.
+# 提取搜索结果链接
+linkElems = soup.select('.package-snippet')
+numOpen = min(5, len(linkElems))
+print('Number of links to open:', numOpen)
+
+# 打开前五个搜索结果的链接
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening:', urlToOpen)
+    webbrowser.open(urlToOpen)
+
+
+# 示例例子和结果
+# python html_scrap5_search.py requests
+# python html_scrap5_search.py world
+# Searching for: world
+# Number of links to open: 5
+# Opening: https://pypi.org/project/world/
+# Opening: https://pypi.org/project/pixel2world/
+# Opening: https://pypi.org/project/pic2world/
+# Opening: https://pypi.org/project/pixel-world/
+# Opening: https://pypi.org/project/print-0-world/
